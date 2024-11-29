1cd7f30bcb4ed50bbd6e2d433083f408117397b5
Pricecito <matas.gutierrez.1254@gmail.com>
2024-11-29 14:29:38 -0500

    EndPointGet

---
+++ src/main/java/com/apirest/demo/controllers/ProductController.java
@@ -0,0 +1,21 @@
+package com.apirest.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apirest.demo.service.ProductsServiceImpl;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+
+    ProductsServiceImpl productService = new ProductsServiceImpl();
+
+    @GetMapping
+    public ResponseEntity<?> getProducts() {
+        return ResponseEntity.ok(productService.getProducts());
+    }
+
+}
--- src/main/java/com/apirest/demo/service/ProductsServiceImpl.java
+++ src/main/java/com/apirest/demo/service/ProductsServiceImpl.java
@@ -1,5 +1,20 @@
 package com.apirest.demo.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.apirest.demo.domain.Product;
+
 public class ProductsServiceImpl {
+    // Aqui ira toda la logica de la gestión de los productos
+    List<Product> products = new ArrayList<>(Arrays.asList(
+            new Product(1, "Product1", 10.99, 10),
+            new Product(2, "Product2", 5.99, 25),
+            new Product(3, "Product 3", 299.99, 15)));
+
+    public List<Product> getProducts() {
+        return products;
+    }
 
 }
