ccf484a9805ce4658c3f771f54201fefe0192f08
Pricecito <matas.gutierrez.1254@gmail.com>
2024-11-29 14:55:59 -0500

    Arreglando linea

--- src/main/java/com/apirest/demo/controllers/ProductController.java
+++ src/main/java/com/apirest/demo/controllers/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.apirest.demo.service.ProductService;
+import com.apirest.demo.service.ProductsServiceImpl;
+
 
 @RestController
 @RequestMapping("/product")
