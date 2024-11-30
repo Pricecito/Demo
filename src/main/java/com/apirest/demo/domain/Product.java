3c1e68400f40df977e6e0f7fe141f7ab6a613ebf
Pricecito <matas.gutierrez.1254@gmail.com>
2024-11-30 15:44:14 -0500

    Incorporando JACKSON y contructor por defecto

--- src/main/java/com/apirest/demo/domain/Product.java
+++ src/main/java/com/apirest/demo/domain/Product.java
@@ -7,6 +7,10 @@ public class Product {
     private Double price;
     private Integer stock;
 
+    public Product() {
+
+    }
+
     public Product(Integer id, String nombre, Double precio, Integer stock) {
         this.id = id;
         this.name = nombre;
--- src/main/java/com/apirest/demo/service/ProductServiceJSONImpl.java
+++ src/main/java/com/apirest/demo/service/ProductServiceJSONImpl.java
@@ -1,13 +1,17 @@
 package com.apirest.demo.service;
 
+import java.io.IOException;
 import java.util.List;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.apirest.demo.domain.Product;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+//Primary se usa para resolver conflictos cuando hay multiples beans del mismo tipo en la app indicando cual de ellos debe ser preferido.
+@Primary
 @Service
 public class ProductServiceJSONImpl implements ProductService {
 
@@ -15,18 +19,16 @@ public class ProductServiceJSONImpl implements ProductService {
     @Override
     public List<Product> getProducts() {
 
-        List<Product> products = null;
+        List<Product> products;
 
         try {
             products = new ObjectMapper().readValue(this.getClass().getResourceAsStream("/product.json"),
                     new TypeReference<List<Product>>() {
                     });
-        } catch (Exception e) {
-
-        } finally {
+            return products;
+        } catch (IOException e) {
+            throw new RuntimeException();
         }
-
-        return null;
     }
 
 }
